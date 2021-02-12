ed2f85e38c26dccb65b012d49d160854229d80ae
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-12 14:10:30 +0530

    [Dipesh] modify addedMaximumIntLogic

--- FindMaximumTest/UnitTest1.cs
+++ FindMaximumTest/UnitTest1.cs
@@ -13,9 +13,24 @@ namespace FindMaximumTest
 
         [Test]
         [TestCase(99,43,13)]
-        public void GivenFirst(int num1,int num2,int num3)
+        public void GivenFirstNumMaximum_ReturnMaximum(int num1,int num2,int num3)
         {
             Assert.AreEqual(num1,findMaximum.FindMaximumNumber(num1,num2,num3));
         }
+
+        [Test]
+        [TestCase(55, 99, 34)]
+        public void GivenSecondNumMaximum_ReturnMaximum(int num1, int num2, int num3)
+        {
+            Assert.AreEqual(num2, findMaximum.FindMaximumNumber(num1, num2, num3));
+        }
+
+        [Test]
+        [TestCase(12, 43, 99)]
+        public void GivenThirdNumMaximum_ReturnMaximum(int num1, int num2, int num3)
+        {
+            Assert.AreEqual(num3, findMaximum.FindMaximumNumber(num1, num2, num3));
+        }
+
     }
 }
